e0d5c1999ddf651456ff18ac86633c2174e72fe5
Suelen Peres <suelen.prs@gmail.com>
2022-12-22 11:12:10 -0300

    configurando actions e atualizando rules, domain

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,38 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import requests
+
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "moeda_form"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+
+        moeda = tracker.get_slot("moeda_dois")
+        url = f'https://economia.awesomeapi.com.br/json/last/BRL-{moeda}'
+        usuario = tracker.get_slot('usuario')
+
+        json_data = requests.get(url).json()
+        nome_moeda = json_data[2].get('url')
+        compra = json_data[7].get('url')
+        venda = json_data[8].get('url')
+        variacao = json_data[6].get('url')
+        print(nome_moeda, compra, venda, variacao)
+        resp = f'{usuario}, essas são as informações solicitadas: /n {nome_moeda}, {compra}, {venda}, {variacao}'
+        
+
+
+        dispatcher.utter_message(text = resp)
+
+        return [SlotSet("moeda_dois", url)]
